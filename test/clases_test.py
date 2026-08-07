30ef5b175ddecc7ce2bb82905c9b0f8bdc92bc95
Diego Pasten <diegopasten78@gmail.com>
2026-08-07 19:17:23 -0400

    feat: :test_tube: Se añaden nueva estructura para la prueba de la clase Product

--- test/clases_test.py
+++ test/clases_test.py
@@ -5,6 +5,8 @@ sys.path.append(str(root_path / "src"))
 
 # C04: Imports
 from warehouse.domain import Product, Category
+from warehouse.infrastructure.in_memory_product_repository import InMemoryProductRepository
+from warehouse.infrastructure.services.simple_barcode_generator import SimpleBarcodeGenerator
 
 
 # Encuentra la raíz del proyecto (warehouse-manager) y añade 'src' a sys.path
@@ -15,8 +17,66 @@ category_test = Category("Tecnología", "Productos de tecnología", "tec")
 # Crear productos
 product_1 = Product("iPhone 14 Pro Max", category_test)
 product_2 = Product("Samsung S23", category_test)
+product_3 = Product("MacBook Pro 16", category_test)
+product_4 = Product("Sony WH-1000XM5", category_test)
+product_5 = Product("iPad Air M2", category_test)
+product_6 = Product("PlayStation 5", category_test)
+product_7 = Product("Logitech MX Master 3S", category_test)
 
-# Lista para almacenar datos
+""" product_1._assign_id(1)
+product_1._assign_barcode("0000000001")
+
+product_2._assign_id(2)
+product_2._assign_barcode("0000000002")
+
+product_3._assign_id(3)
+product_3._assign_barcode("0000000003")
+
+product_4._assign_id(4)
+product_4._assign_barcode("0000000004")
+
+product_5._assign_id(5)
+product_5._assign_barcode("0000000005")
+
+product_6._assign_id(6)
+product_6._assign_barcode("0000000006")
+
+product_7._assign_id(7)
+product_7._assign_barcode("0000000007") """
+
+barcode_generator = SimpleBarcodeGenerator()
+
+repository = InMemoryProductRepository(barcode_generator)
+
+repository.save(product_1)
+repository.save(product_2)
+repository.save(product_3)
+repository.save(product_4)
+repository.save(product_5)
+repository.save(product_6)
+repository.save(product_7)
+
+"""  
+Aquí el flujo sería:
+
+Product
+   ↓
+repository.save()
+   ↓
+InMemoryProductRepository
+   ↓
+_products
+"""
+
+product = repository.find_by_name("MacBook Pro 16")
+
+if product is not None:
+    print(f"{product.name} - {product.category.name} - ID[{product.id}] - BARCODE[{product.barcode}]")
+else:
+    print("[NOT FOUND] No se encontró el producto")
+
+
+""" # Lista para almacenar datos
 lista_productos = []
 
 for p in [product_1, product_2]:
@@ -26,4 +86,4 @@ for p in [product_1, product_2]:
         "category_code": p.category.code,
     })
 
-print(lista_productos)
+print(lista_productos) """
