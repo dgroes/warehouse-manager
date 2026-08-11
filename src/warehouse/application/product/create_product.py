a122cde242b966e4671a97011e50427ce62e511c
Diego Pasten <diegopasten78@gmail.com>
2026-08-11 10:02:53 -0400

    feat: :test_tube: Se modifica create_product y se hacen más pruebas

--- src/warehouse/application/product/create_product.py
+++ src/warehouse/application/product/create_product.py
@@ -1,21 +1,43 @@
 from warehouse.domain.repositories.product_repository import ProductRepository
 from warehouse.domain.category import Category
-from warehouse.domain.services.barcode_generator import BarcodeGenerator 
+from warehouse.domain.product import Product
+
 
 # CreateProduct debería pedirle al repositorio que guarde el producto.
 class CreateProduct:
-    def __init__(
-            self, 
-            repository: ProductRepository,
-            barcode_generator: BarcodeGenerator):
-            self._repository = repository
-            self._barcode_generator = barcode_generator
+    def __init__(self, repository: ProductRepository):
+        self._repository = repository
 
-    def execute(
-            self,
-            name: str,
-            category: Category
-        ):
-        pass
+    def execute(self, name: str, category: Category):
+        product = Product(name, category)  # <- Crea el objeto de dominio
 
-    
+        # No guarda en la DB, sino que llama la abstracción `ProductRepository`
+        self._repository.save(product)
+
+        # Retornar el producto
+        return product
+
+# Flujo
+# clases_test.py
+#       │
+#       │ execute("iPhone 14 Pro Max", category)
+#       ▼
+# CreateProduct
+#       │
+#       │ Product(...)
+#       ▼
+# Product
+#       │
+#       │ repository.save()
+#       ▼
+# InMemoryProductRepository
+#       │
+#       ├── ID = 1
+#       ├── Barcode = 0000000001
+#       └── _products.append()
+#       │
+#       ▼
+# return product
+#       │
+#       ▼
+# clases_test.py
--- test/clases_test.py
+++ test/clases_test.py
@@ -1,96 +1,95 @@
 import sys
 from pathlib import Path
+from pprint import pprint
+
 root_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_path / "src"))
 
 # C04: Imports
-from warehouse.domain import Product, Category
-from warehouse.infrastructure.in_memory_product_repository import InMemoryProductRepository
-from warehouse.infrastructure.services.simple_barcode_generator import SimpleBarcodeGenerator
-
+from warehouse.domain import Category
+from warehouse.infrastructure.in_memory_product_repository import (
+    InMemoryProductRepository,
+)
+from warehouse.infrastructure.services.simple_barcode_generator import (
+    SimpleBarcodeGenerator,
+)
+from warehouse.application.product.create_product import CreateProduct
 
 # Encuentra la raíz del proyecto (warehouse-manager) y añade 'src' a sys.path
 
 # Crear la categoría primero
 category_test = Category("Tecnología", "Productos de tecnología", "tec")
 
-# Crear productos
-product_1 = Product("iPhone 14 Pro Max", category_test)
-product_2 = Product("Samsung S23", category_test)
-product_3 = Product("MacBook Pro 16", category_test)
-product_4 = Product("Sony WH-1000XM5", category_test)
-product_5 = Product("iPad Air M2", category_test)
-product_6 = Product("PlayStation 5", category_test)
-product_7 = Product("Logitech MX Master 3S", category_test)
-
-""" product_1._assign_id(1)
-product_1._assign_barcode("0000000001")
-
-product_2._assign_id(2)
-product_2._assign_barcode("0000000002")
-
-product_3._assign_id(3)
-product_3._assign_barcode("0000000003")
-
-product_4._assign_id(4)
-product_4._assign_barcode("0000000004")
-
-product_5._assign_id(5)
-product_5._assign_barcode("0000000005")
-
-product_6._assign_id(6)
-product_6._assign_barcode("0000000006")
-
-product_7._assign_id(7)
-product_7._assign_barcode("0000000007") """
-
+# Crear la infraestructura
 barcode_generator = SimpleBarcodeGenerator()
-
 repository = InMemoryProductRepository(barcode_generator)
 
-repository.save(product_1)
-repository.save(product_2)
-repository.save(product_3)
-repository.save(product_4)
-repository.save(product_5)
-repository.save(product_6)
-repository.save(product_7)
+# Crear caso de uso
+create_product = CreateProduct(repository)
 
-"""  
-Aquí el flujo sería:
+# Creación de producto
+product_1 = create_product.execute(
+    "iPhone 14 Pro Max",
+    category_test
+)
 
-Product
-   ↓
-repository.save()
-   ↓
-InMemoryProductRepository
-   ↓
-_products
-"""
+product_2 = create_product.execute(
+    "Samsung S23",
+    category_test
+)
 
-# Busqueda por nombre:
-#product = repository.find_by_name("MacBook Pro 16")
+product_3 = create_product.execute(
+    "MacBook Pro 16",
+    category_test
+)
 
-# Busqueda por Barcode:
-#product = repository.find_by_barcode("0000000002")
+product_2.disable()
 
-# Desactivar un producto por su barcode:
-product = repository.disable("0000000002")
+product_4 = create_product.execute(
+    "Onyx Boox Page 7",
+    category_test
+)
 
-if product is not None:
-    print(f"{product.name} - {product.category.name} - ID[{product.id}] - BARCODE[{product.barcode}] - ESTADO[{product.active}]")
-else:
-    print("[NOT FOUND] No se encontró el producto")
+disabled_product = repository.disable("0000000003")
+
+disabled_product_2 = repository.disable("9999999999")
 
 
-""" # Lista para almacenar datos
-lista_productos = []
 
-for p in [product_1, product_2]:
-    lista_productos.append({
-        "name": p.name,
-        "category": p.category.name,
-        "category_code": p.category.code,
-    })
+pprint(vars(product_1))
+pprint(vars(product_2))
+pprint(vars(product_3))
+pprint(vars(product_4))
 
-print(lista_productos) """
+print(f"\n{vars(disabled_product)}")
+
+print(f"\n{disabled_product_2}")
+
+product_2.enable()
+print(f"\n {vars(product_2)}")
+
+
+# Flujo
+# clases_test.py
+#       │
+#       │ execute("iPhone 14 Pro Max", category)
+#       ▼
+# CreateProduct
+#       │
+#       │ Product(...)
+#       ▼
+# Product
+#       │
+#       │ repository.save()
+#       ▼
+# InMemoryProductRepository
+#       │
+#       ├── ID = 1
+#       ├── Barcode = 0000000001
+#       └── _products.append()
+#       │
+#       ▼
+# return product
+#       │
+#       ▼
+# clases_test.py
